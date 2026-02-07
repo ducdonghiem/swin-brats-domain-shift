95e1e252207f154c3047df491428ca2598b3ded2
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2026-02-07 14:48:49 -0600

    feat: Initial training suite

---
+++ src/training/train.py
@@ -0,0 +1,35 @@
+import torch
+from trainer import Trainer
+from utils.config import load_config
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.nn import CrossEntropyLoss
+
+def main():
+    train_config = load_config('src/configs/train_config.yml')
+
+    # TODO: Data augmentation
+    # TODO: Data loading
+
+    device = torch.device(train_config['device'] if torch.cuda.is_available() else 'cpu')
+    model = None # TODO: Initialize model
+    loss = CrossEntropyLoss()
+    optimizer = AdamW(model.parameters(), lr=train_config['training']['learning_rate'])
+    scheduler = CosineAnnealingLR(optimizer, T_max=train_config['training']['epochs'])
+    trainer = Trainer(
+        model=model,
+        train_loader=None,
+        val_loader=None,
+        loss_fn=loss,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        device=device,
+        config=train_config
+    )
+
+    history = trainer.train()
+
+    # NOTE: optionally plot metrics after training
+
+if __name__ == "__main__":
+    main()
